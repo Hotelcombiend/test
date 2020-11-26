e26bb533fe86c9b00f37a457056bb3728305f458
Hotelcombiend <71114488+Hotelcombiend@users.noreply.github.com>
2020-11-26 18:19:57 +0700

    Update form_post.php

--- form_post.php
+++ form_post.php
@@ -4,6 +4,6 @@
             Name: <input type="text" name="name" /><br>
             Age: <input type="text" name="age" /><br>
             <input type="submit" />
-        </from>
+            </from>
     </body>
 </html>
